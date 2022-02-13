6bc68719a46141bc3981af51951882cbb081fc83
papkov <mikhail.papkov@gmail.com>
2022-02-13 18:30:00 +0200

    Add bash scripts for experiments

---
+++ experiments/two_pass_inv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Move to the working directory
+if ${PWD##*/} == "experiments" ; then
+    cd ..
+fi
+
+echo "$PWD"
+
+for image in ./ssi/benchmark/images/generic_2d_all/gt/*.png; do
+  [ -e "$image" ] || continue
+  echo "${image##*/}"
+  python main.py image="${image##*/}" two_pass=True inv_mse_before_forward_model=True
+done
